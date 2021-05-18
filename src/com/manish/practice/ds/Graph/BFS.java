d8f080c0659ab4671b5e63f3228734dc6aa58a5e
Manish Ranjan <sendmail2manish@gmail.com>
2021-05-19 00:42:08 +0530

    Added Graph Problem

---
+++ src/com/manish/practice/ds/Graph/BFS.java
@@ -0,0 +1,63 @@
+package com.manish.practice.ds.Graph;
+
+import jdk.nashorn.internal.runtime.regexp.joni.constants.Traverse;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BFS {
+
+    private int V;
+    List<Integer>[] adj;
+
+    BFS(int V){
+        this.V = V;
+        adj = new LinkedList[V];
+        for(int i =0; i < V; i++)
+        {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int v , int e)
+    {
+        adj[v].add(e);
+        adj[e].add(v);
+    }
+    public void BFSTraversal(int v)
+    {
+        LinkedList <Integer> queue = new LinkedList<>();
+        boolean visited [] = new boolean [V];
+        visited[v]= true;
+        queue.add(v);
+       while(!queue.isEmpty()){
+           v = queue.poll();
+           System.out.print(v+  "  " );
+           List <Integer> edge = adj[v];
+           for(int i= 0; i <edge.size(); i++)
+           {
+            if(!visited[edge.get(i)]) {
+                visited[edge.get(i)] = true;
+                queue.add(edge.get(i));
+             //   System.out.println(edge.get(i));
+            }
+           }
+       }
+
+    }
+
+    public static void main(String[] args) {
+        BFS g = new BFS(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.BFSTraversal(2);
+
+    }
+}
--- src/com/manish/practice/ds/Graph/GraphCreation.java
+++ src/com/manish/practice/ds/Graph/GraphCreation.java
@@ -1,6 +1,50 @@
-package com.manish.practice.ds.tree.Graph;
+package com.manish.practice.ds.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class GraphCreation  {
+
+    List<List<Integer>> adj ;
+
+    GraphCreation(int V)
+    {
+        adj = new ArrayList<>(V);
+        for(int i =0 ; i < V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+    }
+    public void addEdge(int v, int e)
+    {
+        adj.get(v).add(e);
+        adj.get(e).add(v);
+    }
+    public void printEdge(){
+
+        for(int i =0 ; i < adj.size(); i++)
+        {
+            for(int j =0; j< adj.get(i).size(); j++)
+            System.out.println(i + " -> "+ adj.get(i).get(j));
+        }
+    }
+
+    public static void main(String[] args) {
+        GraphCreation graph = new GraphCreation(3);
+
+        graph.addEdge(0,1);
+        graph.addEdge(2,1);
+        graph.printEdge();
+
+
+        GraphCreation graph1 = new GraphCreation(2);
+
+        graph1.addEdge(0,1);
+//        graph1.addEdge(2,1);
+        graph1.printEdge();
+
+
+    }
 
 
 }
