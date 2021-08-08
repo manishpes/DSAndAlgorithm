bbc192c6de5b8826224cda69a91f4ad1153781ef
Manish Ranjan <42301730+manishpes@users.noreply.github.com>
2021-08-08 16:55:54 +0530

    Update JumpGame2.java

--- src/com/manish/practice/algorithm/leetcode/JumpGame2.java
+++ src/com/manish/practice/algorithm/leetcode/JumpGame2.java
@@ -20,7 +20,35 @@ public class JumpGame2 {
                 dp[i] = Math.min(dp[i], 1+dp[j]);
             }
         }
-        return dp[0];*/
+        return dp[0];
+        
+        
+        
+        /*int n = nums.length;
+        int k = 4;
+        int [] dp = new  int [n];
+        Arrays.fill(dp, Integer.MAX_VALUE -1);
+        dp[0] = 1;
+        for(int i = 0; i <n; i++)
+        {
+            if(nums[i] != 0){
+                for(int j = i+1; j<= i+k ; j++)
+                {
+                    if(j>=n)
+                        break;
+                    dp[j] = Math.min(dp[j], 1+dp[i]);
+                }
+            }
+        }
+        if(dp[n-1]== Integer.MAX_VALUE || dp[n-1] == Integer.MAX_VALUE-1)
+        {
+            return -1;
+        }
+        return dp[n-1];*/
+        
+        
+        
+        */
 
         int jumps = 0;
         int farthest = 0;
